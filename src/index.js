1c333783287e7719c6a4aa3a7f47d445a0f31e24
Sam Silver <samsilver@wustl.edu>
2019-05-16 15:58:54 -0700

    test

--- src/index.js
+++ src/index.js
@@ -85,6 +85,6 @@ const server = new GraphQLServer({
 })
 
 
-// ----------------------------------
+// --------------------
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
